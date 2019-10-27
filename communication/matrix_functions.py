fcd3671c8557fe8f1aae5fe0e019c24bf4cbb315
Manegok <jakubreszka97@gmail.com>
2019-10-27 16:21:58 +0100

    transpose fix

--- communication/matrix_functions.py
+++ communication/matrix_functions.py
@@ -1,7 +1,8 @@
 import numpy as np 
 
 def transpose(matrix):
-    return np.matrix.transpose(matrix)
+    n_matrix = np.array(matrix)
+    return np.matrix.transpose(n_matrix)
 
 def inverse(matrix):
     return np.linalg.inv(matrix)
