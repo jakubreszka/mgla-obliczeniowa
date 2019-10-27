7a20f0c785046085c1a8a50f4fd21bbd6b03109d
Manegok <jakubreszka97@gmail.com>
2019-10-27 13:31:14 +0100

    proba wyslania wiadomosci testowej na klienta

--- communication/server.py
+++ communication/server.py
@@ -23,7 +23,7 @@ while 1:
         # print(tosend)
         # serv_socket.send(tosend)
         tosend = testtt.message('XD')
-        serv_socket.send(tosend)
+        client_socket.send(tosend)
     else:
         print(data)
     #client_socket.close()
