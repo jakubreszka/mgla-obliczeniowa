ee0fa8d74a539eb88766aacfb74227166a426671
Manegok <jakubreszka97@gmail.com>
2019-10-27 16:43:22 +0100

    test transponowanej macierzy

--- communication/server.py
+++ communication/server.py
@@ -20,6 +20,7 @@ while 1:
         client_socket.send(tosend)
     elif data == 'transpose':
         tosend = matrix_functions.transpose([[1,2],[3,4]])
+        print(tosend)
         client_socket.send(tosend)
     else:
         print(data)
