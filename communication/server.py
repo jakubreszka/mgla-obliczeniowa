f7bc63ee9d052eaa2fde25367152448da9376cdc
Manegok <jakubreszka97@gmail.com>
2019-10-27 16:20:17 +0100

    dodanie transpozycji

--- communication/server.py
+++ communication/server.py
@@ -18,6 +18,9 @@ while 1:
     if data == 'runtest':
         tosend = testtt.message('XD')
         client_socket.send(tosend)
+    elif data == 'transpose':
+        tosend = matrix_functions.transpose([[1,2],[3,4]])
+        client_socket.send(tosend)
     else:
         print(data)
     client_socket.close()
