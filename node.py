467580ef751731e2f782050f5251f203d06448ea
Manegok <jakubreszka97@gmail.com>
2019-12-05 03:02:13 +0100

    obsluga komunikacji na jsonie

--- node.py
+++ node.py
@@ -1,25 +1,35 @@
 import socket
 import matrix_functions
+import numpy as np
+import json
 
 server_ip   = 'localhost'
 port_number = 4000
 SIZE = 1024
 node_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tosend = bytes(0)
+tosend = {}
 disconnect = False
 try:
     node_socket.connect((server_ip, port_number))
     print(f'Node otrzymuje polecenia z serwera o adresie: {server_ip}:{port_number}')
     while disconnect is False:
-        request = node_socket.recv(SIZE).decode('utf-8')
-        print(f'Otrzymano polecenie {request}')
-        if request == 'transpose':
-            tosend = matrix_functions.transpose([[1,2],[3,4]]).encode('utf-8')
+        package = node_socket.recv(SIZE).decode('utf-8')
+        package_json = json.loads(package)
+        print(f'Otrzymano polecenie {package_json["request"]}')
+        print('Macierz przed wykonaniem polecenia: ')
+        print(package_json['data'])
+        tosend['recieving_client'] = package_json['sender']
+        if package_json['request'] == 'transpose':
+            tosend['answer'] = matrix_functions.transpose(package_json['data'])
+        elif package_json['request'] == 'inverse':
+            tosend['answer'] = matrix_functions.inverse(package_json['data'])
         else:
             print('Nieznane polecenie!')
-        print(tosend)
-        node_socket.send(tosend)
-        #disconnect = True
+        print('Macierz po wykonaniu polecenia: ')
+        print(tosend['answer'])
+        tosend_json = json.dumps(tosend)
+        node_socket.send(tosend_json.encode('utf-8'))
+        disconnect = True
 except socket.error:
     pass
 finally:
