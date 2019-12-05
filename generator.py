c673cbefd0c182b9995fa41057d474f665d85fd1
Manegok <jakubreszka97@gmail.com>
2019-12-05 03:01:03 +0100

    generowanie listy do jsona

--- generator.py
+++ generator.py
@@ -1,4 +1,7 @@
 import numpy as np
+import json
 
 array = np.random.randint(low=-10, high=10, size=(4,4))
-savedarray = np.savetxt('tablefile.txt', array, fmt='%d')
+array_list = array.tolist()
+with open('tablefile.txt', 'w+') as f:
+    json.dump(array_list, f)
