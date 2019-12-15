c851acac3819d92ac3d8b5273e442cc8cee94786
Manegok <jakubreszka97@gmail.com>
2019-12-15 12:17:32 +0100

    kolejkowanie zadan na serwerze

--- generator.py
+++ generator.py
@@ -3,5 +3,8 @@ import json
 
 array = np.random.randint(low=-10, high=10, size=(4,4))
 array_list = array.tolist()
-with open('tablefile.txt', 'w+') as f:
+print('Podaj nazwe pliku wynikowego: ', end='')
+name = input()
+filename = name + '.txt'
+with open(filename, 'w+') as f:
     json.dump(array_list, f)
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import matrix_functions
 import threading
 import json
 import queue
+import time
 
 class FogServer():
     clients = []
@@ -15,7 +16,7 @@ class FogServer():
     idle_nodes = []
     request = ''
     requests = queue.Queue()
-    nodes_queue = queue.Queue()
+    nodes_queue = []
 
     def __init__(self, hostname=socket.gethostbyname(socket.gethostname()), clientport=5000, nodeport=4000, size=1024, fogsize=10):
         #przypisanie adresu ip i portow do komunikacji
@@ -42,44 +43,50 @@ class FogServer():
         print()
         client_listener = threading.Thread(target= self.acceptclients)
         node_listener = threading.Thread(target= self.acceptnodes)
+        client_remover = threading.Thread(target= self.remove_dead_clients)
+        node_remover = threading.Thread(target= self.remove_dead_nodes)
         client_listener.start()
         node_listener.start()
+        client_remover.start()
+        node_remover.start()
         client_listener.join()
         node_listener.join()
+        client_remover.join()
+        node_remover.join()
 
     def acceptclients(self):
         while True:
             client_socket, client_address = self.clientsocket.accept()
-            print(f'Klient podłączył się z adresu: {client_address}')
+            self.addclient(client_socket)
+            print(f'\nKlient podłączył się z adresu: {client_address}')
             self.client_threads.append(threading.Thread(target= self.clientconnection, args=(client_socket, client_address)))
             self.client_threads[-1].start()
-            self.addclient(client_socket)
             self.showclients()
 
     def acceptnodes(self):
         while True:
             node_socket, node_address = self.nodesocket.accept()
-            print(f'Dodano węzeł pod adresem: {node_address}')
-            self.node_threads.append(threading.Thread(target= self.nodeconnection, args=(node_socket, node_address)))
-            #self.node_threads.append(threading.Thread(target= self.nodeconnection, args=()))
-            self.node_threads[-1].start()
             self.addnode(node_socket)
-            self.nodes_queue.put((node_socket, node_address))
+            print(f'\nDodano węzeł pod adresem: {node_address}')
             self.shownodes()
+            #self.node_threads.append(threading.Thread(target= self.nodeconnection, args=(node_socket, node_address)))
+            self.node_threads.append(threading.Thread(target= self.nodeconnection, args=()))
+            self.node_threads[-1].start()
+            self.nodes_queue.insert(0, (node_socket, node_address))
+            print('\nStatus kolejki: \n' + str(self.nodes_queue))
 
     def clientconnection(self, client, address):
         tosend = {}
         while True:
             try:
                 self.request = client.recv(self.size).decode('utf-8')
-                print(f'Otrzymano polecenie {self.request} od klienta: {address}')
+                print(f'\nOtrzymano polecenie {self.request} od klienta: {address}')
                 request_json = json.loads(self.request)
                 self.requests.put(request_json)
                 if request_json['request'] == 'disconnect':
                     print(f'{address} rozłączył się')
-                    client.close()
                     self.clients.remove(client)
-                    self.showclients()
+                    client.close()
                 #czesc wyzej wykonuje sie przed dodaniem noda
                 #petla while wykonuje sie w trakcie obslugi polaczenia noda
                 #kiedy znajduje traf, przerywa sie, konczy sie kod noda i wykonuje pozostaly tej funkcji
@@ -87,36 +94,75 @@ class FogServer():
                 while True:
                     if request_json['sender'] in self.answers:
                         tosend = self.answers[request_json['sender']]
-                        print(f'Odpowiedź do wysłania klientowi: {tosend}')
                         self.answers.pop(request_json['sender'])
                         break 
                 tosend_json = json.dumps(tosend)
-                print('Wysyłam odpowiedź do klienta')
+                print('\nWysyłam odpowiedź do klienta\n')
                 client.send(tosend_json.encode('utf-8'))
             except:
+                self.clients.remove(client)
                 client.close()
                 return False
     
-    def nodeconnection(self, node, address):
+    def nodeconnection(self):
         while True:
             try:
-                print(f'Wybrany node: {node}, na adresie {address}')
                 #czekanie na przyslanie nowego przed dalsza egzekucja kodu
                 while True:
                     if self.request != '':
                         break
-                print(f'Wysyłam polecenie {self.request} na węzeł {address}')
-                node.send(self.request.encode('utf-8'))
+                node, address = self.select_node()
+                #node, address = self.nodes_queue[0]
+                #del self.nodes_queue[0]
+                #self.nodes_queue.append((node, address))
+                # print('\nStatus kolejki: \n' + str(self.nodes_queue))
+                package = self.requests.get()
+                print(f'\nWysyłam polecenie na węzeł {address}')
+                node.send(json.dumps(package).encode('utf-8'))
                 self.request = ''
                 self.answer = node.recv(self.size).decode('utf-8')
-                print(f'Odpowiedź z węzła: {self.answer}')
+                print('\nOdpowiedź z węzła: \n' + str(self.answer))
                 answer_json = json.loads(self.answer)
+                self.answer = ''
                 self.answers[answer_json['recieving_client']] = answer_json
+                #del self.nodes_queue[0]
+                #self.nodes_queue.append((node, address))
+                print('\nStatus kolejki: \n' + str(self.nodes_queue))
             except socket.error as exc:
                 print(str(exc))
                 node.close()
+                self.nodes.remove(node)
                 return False
         node.close()
+        self.nodes.remove(node)
+
+    def select_node(self):
+        node, address = self.nodes_queue[0]
+        del self.nodes_queue[0]
+        self.nodes_queue.append((node, address))
+        return node, address
+
+    def remove_dead_clients(self):
+        while True:
+            time.sleep(5)
+            for thread in self.client_threads:
+                if not thread.is_alive():
+                    self.client_threads.remove(thread)
+                    print('Usunięto martwy wątek klienta')
+                    print(thread)
+                    print('Aktywne watki klientów')
+                    print(self.client_threads)
+    
+    def remove_dead_nodes(self):
+        while True:
+            time.sleep(5)
+            for thread in self.node_threads:
+                if not thread.is_alive():
+                    self.node_threads.remove(thread)
+                    print('Usunięto martwy wątek węzła')
+                    print(thread)
+                    print('Aktywne watki węzłów')
+                    print(self.node_threads)
     
     def addclient(self, client):
         if client not in self.clients:
@@ -128,7 +174,7 @@ class FogServer():
         if node not in self.nodes:
             self.nodes.append(node)
         else:
-            print('Węzeł znajduje się już w mgle')
+            print('Węzeł jest już połączony')
                 
     def showclients(self):
         print('Aktualnie połączeni klienci: ')
@@ -139,8 +185,7 @@ class FogServer():
         print('Aktualnie dodane węzły: ')
         for node in self.nodes:
             print(node)
-
-
+    
 if __name__ == "__main__":
     server = FogServer(hostname='localhost')
     server.acceptconnections()
