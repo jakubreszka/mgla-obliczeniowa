dc5d871646f4f9270e4f58e730265be15ce9974c
Manegok <jakubreszka97@gmail.com>
2019-12-18 17:24:18 +0100

    usuniecie f-string

--- node.py
+++ node.py
@@ -4,7 +4,7 @@ import json
 import queue
 import threading
 
-server_ip   = 'localhost'
+server_ip   = '192.168.1.9'
 port_number = 4000
 SIZE = 65535
 request_queue = queue.Queue()
@@ -14,7 +14,7 @@ def run_node(ip, port, size):
         #utworzenie socketu łączącego się z serwerem i połączenie się z serwerem
         node_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         node_socket.connect((server_ip, port_number))
-        print(f'Node otrzymuje polecenia z serwera o adresie: {server_ip}:{port_number}')
+        print('Node otrzymuje polecenia z serwera o adresie: ' + server_ip + ':' + port_number, end='')
         #uruchomienie wątku obsługującego odbiór zapytań z serwear
         runner = threading.Thread(target=getrequests, args=(node_socket, size))
         runner.start()
@@ -23,7 +23,7 @@ def run_node(ip, port, size):
                 tosend = {}
                 #pobranie zapytania z kolejki
                 package_json = json.loads(request_queue.get())
-                print(f'Otrzymano polecenie {package_json["request"]}')
+                print('Otrzymano polecenie: ' + package_json['request'])
                 print('Macierz przed wykonaniem polecenia: ')
                 print(package_json['data'])
                 tosend['receiving_client'] = package_json['sender']
